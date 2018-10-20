88876a40297d32f96249c167a9aae10e010bae1e
Niloufar Mazloumpour <niloufar@mazloumpour.net>
2018-10-20 17:18:35 -0300

    Parametrized test

--- java-core/src/datastructure/TextAnalyzerTest.java
+++ java-core/src/datastructure/TextAnalyzerTest.java
@@ -1,40 +1,42 @@
 package datastructure;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Collection;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.Test;
+import org.junit.Before;
+
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
 
 /**
  * @author Niloufar Mazloumpour
  * @mail niloufar@mazloumpour.net
  * @since 2018-05-22
  */
+@RunWith(Parameterized.class)
 public class TextAnalyzerTest {
-    @Test
-    public void testOutput() {
-        TextAnalyzer textAnalyzer = new TextAnalyzer();
+    private TextAnalyzer textAnalyzer;
+    private String inputText;
+    private WordInfo wordInfo;
 
-        System.out.println();
-        String testString = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+    @Before
+    public void initialize() {
+        textAnalyzer = new TextAnalyzer();
+    }
 
-        WordInfo mostRepeatedWordInfo = textAnalyzer.findMostRepeatedWord(testString);
-        assertEquals("lorem", mostRepeatedWordInfo.getWord());
-        assertEquals(4, mostRepeatedWordInfo.getRepeatedTimes());
+    public TextAnalyzerTest(String inputText, WordInfo wordInfo) {
+        this.inputText = inputText;
+        this.wordInfo = wordInfo;
+    }
 
-        System.out.println();
+    @Parameterized.Parameters
+    public static Collection primeNumbers() {
+        String testString1 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
         String testString2 = "Penguins are aquatic, flightless birds that are highly adapted to life in the water. Their distinct tuxedo-like appearance is called countershading, a form of camouflage that helps keep them safe in the water. Penguins do have wing-bones, though they are flipper-like and extremely suited to swimming. Penguins are found almost exclusively in the southern hemisphere, where they catch their food underwater and raise their young on land.";
-        mostRepeatedWordInfo = textAnalyzer.findMostRepeatedWord(testString2);
-        assertEquals("penguins", mostRepeatedWordInfo.getWord());
-        assertEquals(3, mostRepeatedWordInfo.getRepeatedTimes());
-
-
-        System.out.println();
         String testString3 = "Students seek relief from rising prices through the purchase of used copies of textbooks, which tend to be less expensive. Most college bookstores offer used copies of textbooks at lower prices. Most bookstores will also buy used copies back from students at the end of a term if the book is going to be re-used at the school. Books that are not being re-used at the school are often purchased by an off-campus wholesaler for 0-30% of the new cost, for distribution to other bookstores where the books will be sold. Textbook companies have countered this by encouraging faculty to assign homework that must be done on the publisher's website. If a student has a new textbook, then he or she can use the pass code in the book to register on the site. If the student has purchased a used textbook, then he or she must pay money directly to the publisher in order to access the website and complete assigned homework. ";
-        mostRepeatedWordInfo = textAnalyzer.findMostRepeatedWord(testString3);
-        assertEquals("used", mostRepeatedWordInfo.getWord());
-        assertEquals(6, mostRepeatedWordInfo.getRepeatedTimes());
-
-        System.out.println();
         String testString4 = "Sunday morning rain is falling\n" +
                 "Steal some covers share some skin\n" +
                 "Clouds are shrouding us in moments unforgettable\n" +
@@ -80,9 +82,21 @@ public class TextAnalyzerTest {
                 "I'm a flower in your hair.\n" +
                 "\n" +
                 "Oh, yeah, yeah, oh\n";
-        mostRepeatedWordInfo = textAnalyzer.findMostRepeatedWord(testString4);
-        assertEquals("sunday", mostRepeatedWordInfo.getWord());
-        assertEquals(5, mostRepeatedWordInfo.getRepeatedTimes());
 
+        return Arrays.asList(new Object[][]{
+                {testString1, new WordInfo("lorem", 4)},
+                {testString2, new WordInfo("penguins", 3)},
+                {testString3, new WordInfo("used", 6)},
+                {testString4, new WordInfo("sunday", 5)},
+        });
+    }
+
+    @Test
+    public void testOutput() {
+        System.out.println();
+
+        WordInfo mostRepeatedWordInfo = textAnalyzer.findMostRepeatedWord(inputText);
+        assertEquals(wordInfo.getWord(), mostRepeatedWordInfo.getWord());
+        assertEquals(wordInfo.getRepeatedTimes(), mostRepeatedWordInfo.getRepeatedTimes());
     }
 }
